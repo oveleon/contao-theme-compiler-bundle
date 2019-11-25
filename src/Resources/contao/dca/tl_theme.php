42dde317435d797d9bafde61aa8735287f0b4e10
doishub <daniele@oveleon.de>
2019-11-25 10:26:49 +0100

    Add a quick link to compile the theme Add translations

--- src/Resources/contao/dca/tl_theme.php
+++ src/Resources/contao/dca/tl_theme.php
@@ -1,13 +1,11 @@
 <?php
-
-// ToDO: Quick Link for Theme each theme to compile
-
 // Add operation
-$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['themeCompiler'] = array
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['compileConfig'] = array
 (
-    'label'               => &$GLOBALS['TL_LANG']['tl_theme']['themeCompiler'],
-    'href'                => 'table=tl_theme_compiler',
-    'icon'                => 'bundles/contaothemecompiler/icons/wand.svg',
+    'label'               => &$GLOBALS['TL_LANG']['tl_theme']['compileConfig'],
+    'href'                => 'act=compile&do=maintenance',
+    'icon'                => 'bundles/contaothemecompiler/icons/compile.svg',
+    'button_callback'     => array('tl_theme_compiler', 'compileThemeStyles')
 );
 
 // Add fields
@@ -38,8 +36,43 @@ $GLOBALS['TL_DCA']['tl_theme']['fields']['outputFilesTargetDir'] = array
     'sql'                     => "blob NULL"
 );
 
+
 // Extend the default palette
 Contao\CoreBundle\DataContainer\PaletteManipulator::create()
     ->addLegend('compiler_legend', 'vars_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_BEFORE)
     ->addField(array('skinSourceFiles', 'outputFilesTargetDir', 'combineSkinFiles'), 'compiler_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('default', 'tl_theme');
+
+/**
+ * Provide miscellaneous methods that are used by the data configuration array.
+ *
+ * @author Daniele Sciannimanica <daniele@oveleon.de>
+ */
+class tl_theme_compiler extends \Backend
+{
+    /**
+     * Import the back end user object
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->import('BackendUser', 'User');
+    }
+
+    /**
+     * Return the "import theme" link
+     *
+     * @param string $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function compileThemeStyles($row, $href, $label, $title, $icon, $attributes)
+    {
+        return '<a href="' . $this->addToUrl($href)  . '&amp;theme=' . $row['id'] . '" title="' . StringUtil::specialchars($title) . '"' . $attributes . '>' . Image::getHtml($icon, $label) . '</a> ';
+    }
+}
